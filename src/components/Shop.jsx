a2e84ada5f1afd87aed09c94d8d7c491cd011ed8
Flaryiest <ericmzuo1@gmail.com>
2024-07-15 14:46:42 -0600

    generated items using fakestore api

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,23 @@
+import { useState, useEffect } from "react"
 import { useOutletContext } from "react-router-dom"
-import Header from "./Header"
-
+import Shopcard from "./Shopcard"
+import "../styles/Shop.css"
 function Shop() {
-    let shopData = fetch('https://fakestoreapi.com/products/')
-    .then(res=>res.json())
-    .then(json=>console.log(json))
-    console.log(shopData)
+    const [shopItems, setShopItems] = useState([])
+
+    useEffect(() => {
+        getShopData()
+    }, [])
+    async function getShopData() {
+        let shopDataRequest = await fetch('https://fakestoreapi.com/products/')
+        let shopData = await shopDataRequest.json()
+        for (let i = 0; i in shopData; i++) {
+            shopData[i].number = 0
+        }
+        setShopItems(shopData)
+    }
+    console.log(shopItems)
+    
     let count = useOutletContext()[0]
     let setCount = useOutletContext()[1]
     let items = useOutletContext()[2]
@@ -15,8 +27,10 @@ function Shop() {
     function handleClick() {
         setCount(count + 1)
     }
-    return <div>
-    <button type="button" onClick={handleClick}>Test</button>
+    return <div className="shop">
+    {shopItems.map((item) => {
+        return <Shopcard key = {item.id} number = {item.number} name={item.title} image={item.image} price={"$" + item.price}></Shopcard>
+    })}
     </div> 
 }
 
--- src/components/Shopcard.jsx
+++ src/components/Shopcard.jsx
@@ -1,1 +1,11 @@
-function Shopcard
+import "../styles/Shopcard.css"
+
+function Shopcard({number, name, image, price}) {
+    return <div className="Shopcard"> 
+        <img src={image}></img>
+        <h2>{name}</h2>
+        <h2>{price}</h2>
+    </div>
+}
+
+export default Shopcard
